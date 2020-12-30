6644f4657051c46f4f3f1a40fd31f9d5a3511304
UnDesSix <facebook.matthieu@yahoo.fr>
2020-12-30 08:48:23 +0100

    Fonction intersections sphere

---
+++ srcs/sphere.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sphere.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/16 14:21:37 by mlarboul          #+#    #+#             */
+/*   Updated: 2020/12/21 17:29:17 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/mini_rt.h"
+
+void	ft_sphere(t_mini_rt *rt, t_obj *sphere, t_vec ori, t_vec dir)
+{
+	t_solve	s;
+	float	delta;
+
+	s.sub = vec_sub(ori, sphere->point1);
+	s.a = 1;
+	s.b = 2 * vec_dot(dir, s.sub);
+	s.c = pow(vec_length(s.sub), 2) - pow(sphere->diameter / 2, 2);
+	delta = pow(s.b, 2) - 4 * s.a *s.c;
+	if (delta < 0)
+		return ;
+	s.t1 = (-s.b - sqrt(delta)) / 2 * s.a;
+	s.t2 = (-s.b + sqrt(delta)) / 2 * s.a;
+//	rt->flag = 0;
+	if (s.t1 >= 0 && s.t1 < rt->t)
+	{
+		rt->t = s.t1;
+		rt->last_obj = sphere;
+		rt->flag = 0;
+	}
+	else if (s.t2 >= 0 && s.t2 < rt->t)
+	{
+		rt->t = s.t2;
+		rt->last_obj = sphere;
+		rt->flag = 0;
+	}
+}
