f9bcb2f3720ba7820a574b46f622dae6e087ae0a
UnDesSix <facebook.matthieu@yahoo.fr>
2020-12-30 08:49:54 +0100

    Lights et colors - les fichiers portent mal leurs noms

---
+++ srcs/light.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   light.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/16 14:21:02 by mlarboul          #+#    #+#             */
+/*   Updated: 2020/12/24 16:09:23 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/mini_rt.h"
+
+void	amb_light(t_mini_rt *rt)
+{
+	rt->tmp_color.r = min_max_colors((rt->tmp_color.r + rt->amb.ratio
+												* rt->last_obj->color.r));
+	rt->tmp_color.g = min_max_colors((rt->tmp_color.g + rt->amb.ratio
+												* rt->last_obj->color.g));
+	rt->tmp_color.b = min_max_colors((rt->tmp_color.b + rt->amb.ratio
+												* rt->last_obj->color.b));
+}
+
+void	init_color(t_mini_rt *rt)
+{
+	rt->tmp_color.r = 0;
+	rt->tmp_color.g = 0;
+	rt->tmp_color.b = 0;
+}
+
+t_color	color_add(t_color c1, t_color c2)
+{
+	t_color	c;
+
+	c.r = min_max_colors(c1.r + c2.r);
+	c.g = min_max_colors(c1.g + c2.g);
+	c.b = min_max_colors(c1.b + c2.b);
+	return (c);
+}
+
+t_color	color_mult(t_color c1, float nb)
+{
+	t_color	c;
+
+	c.r = min_max_colors(c1.r * nb);
+	c.g = min_max_colors(c1.g * nb);
+	c.b = min_max_colors(c1.b * nb);
+	return (c);
+}
+
+float	min_max_colors(float color)
+{
+	if (color > 255)
+		return (255);
+	else if (color < 0)
+		return (0);
+	return (color);
+}
+
+void	apply_light(t_mini_rt *rt, t_vec ori, t_vec dir, t_light light)
+{
+	if (rt->last_obj->id == SPHERE)
+		sphere_light(rt, ori, dir, light);
+	if (rt->last_obj->id == PLANE)
+		plane_light(rt, ori, dir, light);
+/*
+	if (rt->last_obj->id == SQUARE)
+		square_light(rt, ori, dir, light);
+	if (rt->last_obj->id == CYLINDER)
+		cylinder_light(rt, ori, dir, light);
+*/
+	if (rt->last_obj->id == TRIANGLE)
+		triangle_light(rt, ori, dir, light);
+}
---
+++ srcs/light_apply.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   light_apply.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/21 23:17:11 by mlarboul          #+#    #+#             */
+/*   Updated: 2020/12/26 14:06:00 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/mini_rt.h"
+
+void	sphere_light(t_mini_rt *rt, t_vec ori, t_vec dir, t_light light)
+{
+	t_vec	point;
+	t_vec	v_normal;
+	t_vec	v_light;
+
+	point = vec_add(ori, vec_mult(dir, rt->t));
+	v_normal = vec_normalize(vec_sub(point, rt->last_obj->point1));
+	if (vec_dot(dir, v_normal) > 0)
+//	{
+		v_normal = vec_normalize(vec_mult(v_normal, -1));
+//		v_light = vec_normalize(vec_sub(vec_mult(light.point, -1), point));
+//	}
+//	else
+		v_light = vec_normalize(vec_sub(light.point, point));
+	rt->tmp_color = color_add(rt->tmp_color, color_mult(rt->last_obj->color,
+				(light.ratio * vec_dot(v_normal, v_light) /
+					pow(vec_length(vec_sub(light.point, point)) / 100, 2))));
+//					1)));
+}
+
+void	plane_light(t_mini_rt *rt, t_vec ori, t_vec dir, t_light light)
+{
+	t_vec	point;
+	t_vec	v_normal;
+	t_vec	v_light;
+
+	point = vec_add(ori, vec_mult(dir, rt->t));
+	v_normal = vec_normalize(rt->last_obj->orient);
+	if (vec_dot(dir, v_normal) > 0)
+		v_normal = vec_mult(v_normal, -1);
+	v_light = vec_normalize(vec_sub(vec_mult(light.point, 1), point));
+	rt->tmp_color = color_add(rt->tmp_color, color_mult(rt->last_obj->color,
+				(light.ratio * vec_dot(v_normal, v_light) /
+					pow(vec_length(vec_sub(light.point, point)) / 100, 2))));
+//					1)));
+}
+
+void	triangle_light(t_mini_rt *rt, t_vec ori, t_vec dir, t_light light)
+{
+	t_vec	point;
+	t_vec	v_normal;
+	t_vec	v_light;
+	t_vec	v1;
+	t_vec	v2;
+
+	point = vec_add(ori, vec_mult(dir, rt->t));
+	v1 = vec_sub(rt->last_obj->point3, rt->last_obj->point1);
+	v2 = vec_sub(rt->last_obj->point2, rt->last_obj->point1);
+
+	v_normal = vec_normalize(vec_cross(v1, v2));
+	if (vec_dot(dir, v_normal) > 0)
+		v_normal = vec_mult(v_normal, -1);
+	v_light = vec_normalize(vec_sub(vec_mult(light.point, 1), point));
+	rt->tmp_color = color_add(rt->tmp_color, color_mult(rt->last_obj->color,
+				(light.ratio * vec_dot(v_normal, v_light) /
+					pow(vec_length(vec_sub(light.point, point)) / 100, 2))));
+//					1)));
+}
