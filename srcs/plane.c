db3ab3770b8443a008fd44d44dcd3db4ee07766e
UnDesSix <facebook.matthieu@yahoo.fr>
2020-12-30 08:47:45 +0100

    Fonction intersections plans

---
+++ srcs/plane.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/16 14:21:30 by mlarboul          #+#    #+#             */
+/*   Updated: 2020/12/21 17:40:28 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+ #include "../includes/mini_rt.h"
+
+void	ft_plane(t_mini_rt *rt, t_obj *obj, t_vec ori, t_vec dir)
+{
+	t_solve	s;
+
+	s.a = vec_dot(obj->orient, vec_sub(ori, obj->point1));
+	s.b = vec_dot(obj->orient, dir);
+	if (!s.b)
+		return ;
+	s.t1 = -s.a / s.b;
+//	if (s.t1 >= 0)
+	if (s.t1 >= 0 && s.t1 < rt->t)
+	{
+		rt->t = s.t1;
+		rt->last_obj = obj;
+		rt->flag = 0;
+	}
+}
