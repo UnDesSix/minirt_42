095c37ec7ee272a9078aac455cf270cc9e034700
UnDesSix <facebook.matthieu@yahoo.fr>
2020-12-30 08:47:10 +0100

    Fonctions relatives aux calculs vectoriels

---
+++ srcs/vec.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/16 14:22:58 by mlarboul          #+#    #+#             */
+/*   Updated: 2020/12/22 11:08:19 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/mini_rt.h"
+
+t_vec	vec_add(t_vec v1, t_vec v2)
+{
+	t_vec	v;
+
+	v.x = v1.x + v2.x;
+	v.y = v1.y + v2.y;
+	v.z = v1.z + v2.z;
+	return (v);
+}
+
+t_vec	vec_sub(t_vec v1, t_vec v2)
+{
+	t_vec	v;
+
+	v.x = v1.x - v2.x;
+	v.y = v1.y - v2.y;
+	v.z = v1.z - v2.z;
+	return (v);
+}
+
+t_vec	vec_mult(t_vec v1, float nb)
+{
+	t_vec	v;
+
+	v.x = v1.x * nb;
+	v.y = v1.y * nb;
+	v.z = v1.z * nb;
+	return (v);
+}
+
+t_vec	vec_div(t_vec v1, float nb)
+{
+	t_vec	v;
+
+	v.x = v1.x / nb;
+	v.y = v1.y / nb;
+	v.z = v1.z / nb;
+	return (v);
+}
+
+float	vec_dot(t_vec v1, t_vec v2)
+{
+	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
+}
---
+++ srcs/vec2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec2.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/16 14:22:44 by mlarboul          #+#    #+#             */
+/*   Updated: 2020/12/26 12:11:30 by mlarboul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/mini_rt.h"
+
+t_vec	vec_cross(t_vec v1, t_vec v2)
+{
+	t_vec	v;
+
+	v.x = v1.y * v2.z - v1.z * v2.y;
+	v.y = v1.z * v2.x - v1.x * v2.z;
+	v.z = v1.x * v2.y - v1.y * v2.x;
+	return (v);
+}
+
+t_vec	vec_normalize(t_vec v)
+{
+	t_vec	norm;
+	float	len;
+
+	len = vec_length(v);
+	norm.x = v.x / len;
+	norm.y = v.y / len;
+	norm.z = v.z / len;
+	return (norm);
+}
+
+float	vec_length(t_vec v)
+{
+	return (sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2)));
+}
+
+t_vec	vec_create(float x, float y, float z)
+{
+	t_vec	v;
+
+	v.x = x;
+	v.y = y;
+	v.z = z;
+	return (v);
+}
