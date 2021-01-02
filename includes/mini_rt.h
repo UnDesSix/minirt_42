f0fd9a90a7042cd228343a7a4d7cf20f04d00ccf
UnDesSix <facebook.matthieu@yahoo.fr>
2021-01-02 11:04:47 +0100

    Ombres spheres OK

--- includes/mini_rt.h
+++ includes/mini_rt.h
@@ -6,7 +6,7 @@
 /*   By: mlarboul <mlarboul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 14:42:29 by mlarboul          #+#    #+#             */
-/*   Updated: 2021/01/02 10:14:05 by mlarboul         ###   ########.fr       */
+/*   Updated: 2021/01/02 10:35:35 by mlarboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,9 @@ void		sphere_light(t_mini_rt *rt, t_vec ori, t_vec dir, t_light light);
 void		plane_light(t_mini_rt *rt, t_vec ori, t_vec dir, t_light light);
 void		triangle_light(t_mini_rt *rt, t_vec ori, t_vec dir, t_light light);
 
+int			apply_shaders(t_mini_rt *rt, t_obj *obj, t_vec ori, t_vec dir);
 int			is_visible(t_mini_rt *rt, t_light light, t_vec point);
-double		sphere_shaders(t_mini_rt *rt, t_obj *sphere, t_vec ori, t_vec dir);
+int			sphere_shaders(t_mini_rt *rt, t_obj *sphere, t_vec ori, t_vec dir);
 
 /*
 // Color functions
